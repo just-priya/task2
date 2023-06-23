b43b741e7cf437e8ce088edf6b3e302eb5c279be
Priya v <priyavel7358@gmail.com>
2023-06-23 14:37:04 +0530

    commit changes task2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,154 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { MultiSelect } from "react-multi-select-component";
 
-function App() {
+const App = () => {
+  const [selected, setSelected] = useState([]);
+
+  const [options, setOptions] = useState([
+    {
+      id: 1,
+      label: "priya",
+      dob: "22/05/2000",
+      gender: "female",
+      bp: "120/80",
+      respirationRate: "18",
+      heartRate: 70,
+      value: "priya",
+    },
+    {
+      id: 2,
+      label: "kavitha",
+      dob: "21/05/2000",
+      gender: "female",
+      bp: "180/80",
+      respirationRate: "20",
+      value: "kavitha",
+    },
+    {
+      id: 3,
+      label: "Divya",
+      dob: "20/05/2000",
+      gender: "female",
+      bp: "120/80",
+      respirationRate: "17",
+      value: "Divya",
+    },
+    {
+      id: 4,
+      label: "joe",
+      dob: "18/05/2000",
+      gender: "female",
+      bp: "120/80",
+      respirationRate: "18",
+      value: "joe",
+    },
+    {
+      id: 5,
+      label: "pradeepa",
+      dob: "4/05/2000",
+      gender: "female",
+      bp: "170/80",
+      respirationRate: "22",
+      value: "pradeepa",
+    },
+    {
+      id: 6,
+      label: "lavanya",
+      dob: "3/05/2000",
+      gender: "female",
+      bp: "120/80",
+      respirationRate: "16",
+      value: "lavanya",
+    },
+  ]);
+
+  const numAscending = [...selected].sort((a, b) => (a.id > b.id ? 1 : -1));
+
+  const selectChange = (val) => {
+    let change = [...val].map((ele) => JSON.stringify(ele));
+
+    const filteredOptions = [...options] // duplicate options
+      .filter((e) => {
+        // converting option item to string for compare
+        return !change.includes(JSON.stringify(e));
+      });
+    let initial = [...val, ...filteredOptions];
+    setOptions(initial);
+    setSelected(val);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: "20px" }}>
+      <h1>Select Patient name</h1>
+      <div
+        style={{
+          margin: "50px",
+        }}
+      >
+        <div style={{ width: "30%", marginLeft: "70%" }}>
+          <MultiSelect
+            options={options}
+            value={numAscending}
+            onChange={selectChange}
+            labelledBy="Select"
+          />
+        </div>
+        <div style={{ width: "50%", margin: "10px" }}>
+          {!selected ? (
+            <div>
+              {selected.map((e) => (
+                <>
+                  <label>Patient Name:{e.label}</label>
+                  <br></br>
+                  <label>DOB:{e.dob}</label>
+                  <br></br>
+                  <label>Gender:{e.gender}</label>
+                  <br></br>
+                  {e.bp >= "120/80" ? (
+                    <label style={{ backgroundColor: "yellow", width: "25%" }}>
+                      BP:{e.bp}
+                    </label>
+                  ) : (
+                    <>
+                      <label>BP:{e.bp}</label>
+                      <br></br>
+                    </>
+                  )}
+
+                  <hr></hr>
+                </>
+              ))}
+            </div>
+          ) : (
+            <div>
+              {options.map((e) => (
+                <>
+                  <label>Patient Name:{e.label}</label>
+                  <br></br>
+                  <label>DOB:{e.dob}</label>
+                  <br></br>
+                  <label>Gender:{e.gender}</label>
+                  <br></br>
+                  {e.bp > "120/80" ? (
+                    <label style={{ backgroundColor: "yellow", width: "25%" }}>
+                      BP:{e.bp}
+                    </label>
+                  ) : (
+                    <>
+                      <label>BP:{e.bp}</label>
+                      <br></br>
+                    </>
+                  )}
+                  <hr></hr>
+                </>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+      <div></div>
     </div>
   );
-}
+};
 
 export default App;
